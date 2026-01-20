774de67efcab2df01b19900b3fea1f0f0598c39b
LucasMatuszewski <lucas.matuszewski@gmail.com>
2026-01-20 13:54:30 +0000

    Initial commit

---
+++ src/main/java/com/silkycoders1/jsystemssilkycodders1/JSystemsSilkyCodders1Application.java
@@ -0,0 +1,13 @@
+package com.silkycoders1.jsystemssilkycodders1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JSystemsSilkyCodders1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JSystemsSilkyCodders1Application.class, args);
+	}
+
+}
